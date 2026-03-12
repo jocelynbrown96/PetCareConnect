974c5024f9a9c56959dbce85a5e002b6c7ae8b74
Mackenzie <mackenzie@mcknz.dev>
2026-03-12 11:23:45 -0400

    Initial project push

---
+++ PetCareConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PetCareConnect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

4e966dfe9d35d8133697c8c4571ebb4bb88b879d
Jocelyn Brown <jocelynbrown96@gmail.com>
2026-03-12 13:59:47 -0400

    Updated file directory to include additional controllers and views.

---
+++ PetCareConnect/Views/Pets/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetCareConnect.Views.Pets
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

98c3c0bafa55adabef0fd0f9e116b37d44ff95af
Jocelyn Brown <jocelynbrown96@gmail.com>
2026-03-26 09:23:37 -0400

    minor changes

--- PetCareConnect/Models/ApplicationUser.cs
+++ PetCareConnect/Models/ApplicationUser.cs
@@ -5,11 +5,35 @@ namespace PetCareConnect.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? LastLogin { get; set; }
+        public string FirstName 
+        { 
+            get; 
+            set; 
+        } = string.Empty;
+        public string LastName 
+        { 
+            get; 
+            set; 
+        } = string.Empty;
+        public string Address 
+        { 
+            get; 
+            set; 
+        } = string.Empty;
+        public string Role 
+        { 
+            get; 
+            set; 
+        } = string.Empty;
+        public DateTime CreatedAt 
+        { 
+            get; 
+            set; 
+        } = DateTime.UtcNow;
+        public DateTime? LastLogin 
+        { 
+            get; 
+            set; 
+        }
     }
 }
---
+++ PetCareConnect/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace PetCareConnect.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+    }
+}
